0f066fc26710baad0f2b0ce738361daff59ff14b
Richard Kurwitz <kurwitz3@aol.com>
2020-08-19 19:54:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(array)
+  x = 0
+  z = []
+   while x < array.length
+    yield(array[x])
+    my_collect(array[x]) do |name|
+      name.split(" ").first
+    end
+   x = x + 1 
+ end 
+end
